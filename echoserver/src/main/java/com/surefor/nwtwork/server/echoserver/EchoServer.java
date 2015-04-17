c4320128616caf24e9325765ee49845e8e64b67a
sureforprogrammer <^alx+cha0984>
2015-04-17 02:10:15 -0400

    20150416 Add server.properties

--- echoserver/src/main/java/com/surefor/nwtwork/server/echoserver/EchoServer.java
+++ echoserver/src/main/java/com/surefor/nwtwork/server/echoserver/EchoServer.java
@@ -1,6 +1,11 @@
 package com.surefor.nwtwork.server.echoserver;
 
+import com.surefor.utils.properties.ConfigNotFondException;
+import com.surefor.utils.properties.ProperitesExistException;
+import com.surefor.utils.properties.ProperitesNotFondException;
+import com.surefor.utils.properties.Properties;
 import org.apache.commons.cli.*;
+import org.apache.commons.configuration.ConfigurationException;
 import org.apache.log4j.Logger;
 
 /**
@@ -10,6 +15,7 @@ public class EchoServer {
     private Integer port = 12334 ;
     public static final String OPTION_PORT = "port" ;
     public final static Logger logger = Logger.getLogger(EchoServer.class);
+    public final static String PROPERTIES = "server.properties" ;
     public static void main(String[] args) throws ParseException {
         Options options = new Options();
         options.addOption(EchoServer.OPTION_PORT, true, "listening port number.");
@@ -23,8 +29,19 @@ public class EchoServer {
             String port = cmd.getOptionValue(EchoServer.OPTION_PORT) ;
             logger.info(port);
         } else {
-            HelpFormatter help = new HelpFormatter();
-            help.printHelp("EchoServer", options);
+            Properties properties = Properties.instance() ;
+            try {
+                properties.loadProperties(PROPERTIES);
+                int port = Integer.valueOf(properties.get(PROPERTIES, "server.port")) ;
+            } catch (ProperitesExistException e) {
+                e.printStackTrace();
+            } catch (ConfigurationException e) {
+                e.printStackTrace();
+            } catch (ConfigNotFondException e) {
+                e.printStackTrace();
+            } catch (ProperitesNotFondException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
